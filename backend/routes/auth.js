c88dd5548ac2babe316db6f13db4a497063d7a58
pyto-p <codebypytop@gmail.com>
2024-11-30 20:50:57 +0800

    feat: created register and login API endpoints in the auth.js routes

---
+++ backend/routes/auth.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const salt = await bcrypt.genSalt(10); //Salt for a more robust encryption
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered sucessfully" });
+  } catch (error) {
+    res.status(400).json({ error: "Error registering user" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne( { email });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(400).json({ error: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: "Error logging in" });
+  }
+})
