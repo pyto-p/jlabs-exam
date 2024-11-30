d9b1a21258d8843782d160645ed485d260bf0105
pyto-p <codebypytop@gmail.com>
2024-11-30 20:19:16 +0800

    feat: added MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
