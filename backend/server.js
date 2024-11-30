0ba75411d81da2785304ecddc55f42b6645b66b0
pyto-p <codebypytop@gmail.com>
2024-12-01 02:50:57 +0800

    fix: rebuild

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: "https://jlabs-exam.vercel.app/",
+  origin: "https://jlabs-exam.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
