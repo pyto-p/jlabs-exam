8c254b288de6a868c01e6fe185f57beebb8634d2
pyto-p <codebypytop@gmail.com>
2024-12-01 03:01:57 +0800

    fix: env in frontend

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ky from 'ky';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 function LoginPage() {
   const [email, setEmail] = useState('');
